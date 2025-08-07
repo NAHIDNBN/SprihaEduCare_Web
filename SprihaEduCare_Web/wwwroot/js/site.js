927ad08cf17bf5622f487b76e3f426b37021e12c
NAHIDNBN <nahidnbn05@gamil.com>
2025-08-07 21:09:25 +0600

    Responsive design for mobile.....

--- SprihaEduCare_Web/wwwroot/js/site.js
+++ SprihaEduCare_Web/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    $('.your-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+        arrows: true
+    });
+});
